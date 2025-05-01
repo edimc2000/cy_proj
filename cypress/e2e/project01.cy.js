95f99d2039b5b335a8a7efc2837f1d5d6e8efdb1
Eddie C. <edimc2000@gmail.com>
2025-05-01 09:18:36 -0500

    added task 1 and 2

--- cypress/e2e/project01.cy.js
+++ cypress/e2e/project01.cy.js
@@ -1,15 +1,64 @@
-///< reference types = "cypress" />
+/// <reference types = "cypress" />
 
 describe('template spec', () => {
+
   beforeEach(() => {
     cy.visit('https://www.techglobal-training.com/frontend/form-elements')
 
   })
 
+  it('[TC01] Validate the Contact Us information  ', () => {
+    // 1. Navigate to https://techglobal-training.com/frontend/form-elements
+    // used beforeEach()
 
+    // 2. Validate the heading is “Contact Us”
+    cy.get('.is-size-3').should('have.text', 'Contact Us')
 
-  it('[TC1] Validate the Contact Us information  ', () => {
-    cy.visit('https://www.techglobal-training.com/frontend/form-elements')
-    
+    // 3. Validate the address is “2800 S River Rd Suite 310, Des Plaines, IL 60018”
+    cy.get('#address').should('have.text', '2800 S River Rd Suite 310, Des Plaines, IL 60018')
+
+    // 4. Validate the email is “info@techglobalschool.com"
+    cy.get('#email').should('have.text', 'info@techglobalschool.com')
+
+    // 5. Validate the phone number is “(224) 580-2150”
+    cy.get('#phone-number').should('have.text', '(224) 580-2150')
   })
+
+
+  it('[TC02] Validate the Full name input box  ', () => {
+    // 1. Navigate to https://techglobal-training.com/frontend/form-elements
+    // used beforeEach()
+
+    // 2. Validate that the Full name input box is displayed
+    // 3. Validate that the Full name input box is required
+    cy.get(':first-child > .control > .input').should('be.visible').should('have.attr', 'required')
+    
+    // 4. Validate that the label of the Full name input box is “Full name *”
+    cy.get('[for="name"]').should('have.text', 'Full name *')
+
+    // 5. Validate that the placeholder of the Full name input box is “Enter your full name”
+    cy.get(':first-child > .control > .input').should('have.attr', 'placeholder', 'Enter your full name')
+  })
+
+
+  it('[TC03] Validate the Gender radio button  ', () => {
+    // 1. Navigate to https://techglobal-training.com/frontend/form-elements
+    // used beforeEach()
+
+    // 2. Validate that the Full name input box is displayed
+
+
+    // 3. Validate that the Full name input box is required
+
+
+    // 4. Validate that the label of the Full name input box is “Full name *”
+
+
+    // 5. Validate that the placeholder of the Full name input box is “Enter your full name”
+
+  })
+
+
+
+
 })
