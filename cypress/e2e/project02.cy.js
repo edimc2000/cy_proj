e7ffda839f98ed8af238c19dc705ac9e5914b6cc
Eddie C. <edimc2000@gmail.com>
2025-05-12 22:05:58 -0500

    1- removed js test data file 2- renamed test data variables on Proj02 spec 3- implemented fixture json file 4- created method on the page object

--- cypress/e2e/project02.cy.js
+++ cypress/e2e/project02.cy.js
@@ -1,20 +1,23 @@
 /// <reference types = "cypress" />
 require('cypress-plugin-steps')
 const loginPage = require('../pages/loginFormPage.js')
-const { testData } = require('./data/loginFormTestData.js')
 const elements = loginPage.elements
 
-describe('TG Login Form', () => {
-    beforeEach(() => {
+describe('TG Login Form', function () {
+    beforeEach(function () {
+        loginPage.loadTestData()
+
         cy.visit('https://www.techglobal-training.com/frontend/login')
+
+
     })
 
-    it('[TC01] Validate the login form', () => {
+    it('[TC01] Validate the login form', function () {
         // steps 2 to 7   labels and input boxes 
         elements.getDivLabels().each((el, index) => {
             cy.wrap(el)
                 .should('be.visible')
-                .and('have.text', testData.inputlabels[index])
+                .and('have.text', this.inputLabels[index])
                 .next()
                 .should('be.visible')
                 .and('be.enabled')
@@ -25,43 +28,43 @@ describe('TG Login Form', () => {
         elements.getLoginButton()
             .should('be.visible')
             .and('be.enabled')
-            .and('have.text', testData.loginButtonlabel)
+            .and('have.text', this.loginButtonLabel)
 
         // steps 11 to 13 
         elements.getForgotPasswordLink()
             .should('be.visible')
             .and('have.prop', 'tagName', 'A') // a tag for clickable assertion
-            .and('have.text', testData.forgotLabel)
+            .and('have.text', this.forgotLabel)
             .click()
         elements.getModalTitle().should('be.visible') // secondary assertion for the link, when it's clickable
     })
 
-    it('[TC02] Validate the successful login', () => {
+    it('[TC02] Validate the successful login', function () {
         //steps 2-4 
         cy.step('Login with a valid account')
-        loginPage.login(testData.validUserName, testData.validPassword)
+        loginPage.login(this.validUserName, this.validPassword)
 
         //steps 5 -6 
         cy.step('Validation when logged in ')
-        elements.successMessage().should('be.visible').and('have.text', testData.successMessage)
-        elements.getLogoutButton().should('be.visible').and('be.enabled').and('have.text', testData.logoutButtonlabel)
+        elements.successMessage().should('be.visible').and('have.text', this.successMessage)
+        elements.getLogoutButton().should('be.visible').and('be.enabled').and('have.text', this.logoutButtonLabel)
     })
 
 
-    it('[TC03] Validate logout', () => {
+    it('[TC03] Validate logout', function () {
         //steps 2-4
         cy.step('Login with a valid account')
-        loginPage.login(testData.validUserName, testData.validPassword)
+        loginPage.login(this.validUserName, this.validPassword)
 
         //step 5
-        elements.getLogoutButton().click()
+        loginPage.clickLogout()
 
         //step 6
         elements.getUserNameInputbox().should('be.visible')
         elements.getPasswordInputbox().should('be.visible')
     })
 
-    it('[TC04] Validate the Forgot Password? Link and Reset Password modal', () => {
+    it('[TC04] Validate the Forgot Password? Link and Reset Password modal', function () {
         elements.getForgotPasswordLink().click()
 
         //steps 3-6 
@@ -69,7 +72,7 @@ describe('TG Login Form', () => {
         elements.getModalCloseButton().should('be.visible').and('have.attr', 'class', 'delete')
         elements.getModalEmailInputbox().should('be.visible')
         elements.getModalEmailInputbox().parent()
-            .should('have.text', testData.paswordResetLabel)
+            .should('have.text', this.paswordResetLabel)
 
         //steps 7-9
         elements.getModalSubmitButton()
@@ -78,61 +81,62 @@ describe('TG Login Form', () => {
             .and('not.have.attr', 'disabled')
     })
 
-    it('[TC05] Validate the Reset Password modal close button', () => {
+    it('[TC05] Validate the Reset Password modal close button', function () {
         elements.getForgotPasswordLink().click()
 
         //steps 3-5
-        elements.getModalTitle().should('have.text', testData.modelResetPasswordTitle)
+        elements.getModalTitle().should('have.text', this.modalResetPasswordTitle)
         elements.getModalCloseButton().should('be.visible').click()
         elements.getModalCard().should('not.exist')
     })
 
-    it('[TC06] Validate the Reset Password form submission', () => {
+    it('[TC06] Validate the Reset Password form submission', function () {
         //steps 2-6
-        loginPage.passwordReset(testData.emailAddress)
+        loginPage.passwordReset(this.emailAddress)
         elements.getModalConfirmationMessage()
             .should('be.visible')
-            .and('have.text', testData.paswordResetSumittedLabel)
+            .and('have.text', this.paswordResetSumittedLabel)
     })
 
 
-    it('[TC07] Validate the invalid login with the empty credentials', () => {
+    it('[TC07] Validate the invalid login with the empty credentials', function () {
         //steps 2-4
         loginPage.loginEmpty()
 
         //step 5
         elements.errorMessage()
             .should('be.visible')
-            .and('have.text', testData.errorMessageUsername)
+            .and('have.text', this.errorMessageUsername)
     })
 
-    it('[TC08] Validate the invalid login with the wrong username', () => {
+    it('[TC08] Validate the invalid login with the wrong username', function () {
         //steps 2-4
-        loginPage.login(testData.invalidUserName, testData.validPassword)
+        loginPage.login(this.invalidUserName, this.validPassword)
 
         //step 5
         elements.errorMessage()
             .should('be.visible')
-            .and('have.text', testData.errorMessageUsername)
+            .and('have.text', this.errorMessageUsername)
     })
 
-    it('[TC09] Validate the invalid login with the wrong password', () => {
+    it('[TC09] Validate the invalid login with the wrong password', function () {
         //steps 2-4
-        loginPage.login(testData.validUserName, testData.invalidPassword)
+        loginPage.login(this.validUserName, this.invalidPassword)
 
         //step 5
         elements.errorMessage()
             .should('be.visible')
-            .and('have.text', testData.errorMessagePassword)
+            .and('have.text', this.errorMessagePassword)
     })
 
-    it('[TC10] Validate the invalid login with the wrong username and password', () => {
+    it('[TC10] Validate the invalid login with the wrong username and password', function () {
         //steps 2-4
-        loginPage.login(testData.invalidUserName, testData.invalidPassword)
+        loginPage.login(this.invalidUserName, this.invalidPassword)
 
         //step 5
         elements.errorMessage()
             .should('be.visible')
-            .and('have.text', testData.errorMessageUsername)
+            .and('have.text', this.errorMessageUsername)
     })
+
 })
--- cypress/pages/loginFormPage.js
+++ cypress/pages/loginFormPage.js
@@ -17,26 +17,58 @@ class LoginPage {
         getModalSubmitButton: () => cy.get('#submit'),
         getModalConfirmationMessage: () => cy.get('#confirmation_message'),
 
-        getDivLabels : () => cy.get('div label'),
+        getDivLabels: () => cy.get('div label'),
     }
 
-    login(username, password){
+
+    clickLogin() {
+        this.elements.getLoginButton().click()
+    }
+
+    clickLogout() {
+        this.elements.getLogoutButton().click()
+    }
+
+    login(username, password) {
         this.elements.getUserNameInputbox().type(username).should('have.value', username)
         this.elements.getPasswordInputbox().type(password).should('have.value', password)
-        this.elements.getLoginButton().click()
+        this.clickLogin()
     }
 
     loginEmpty() {
         this.elements.getUserNameInputbox().clear()
         this.elements.getPasswordInputbox().clear()
-        this.elements.getLoginButton().click()
+        this.clickLogin()
     }
-    
+
     passwordReset(email) {
         this.elements.getForgotPasswordLink().click()
         this.elements.getModalEmailInputbox().type(email).should('have.value', email)
         this.elements.getModalSubmitButton().click()
     }
+
+    loadTestData() {
+        cy.fixture('loginFormTestData.json').then(function (data) {
+            this.invalidUserName = data['invalidUserName']
+            this.invalidPassword = data['invalidPassword']
+            this.emailAddress = data['emailAddress']
+            this.inputLabels = data['inputLabels']
+            this.loginButtonLabel = data['loginButtonLabel']
+            this.logoutButtonLabel = data['logoutButtonLabel']
+            this.forgotLabel = data['forgotLabel']
+            this.successMessage = data['successMessage']
+            this.errorMessageUsername = data['errorMessageUsername']
+            this.errorMessagePassword = data['errorMessagePassword']
+            this.paswordResetLabel = data['paswordResetLabel']
+            this.paswordResetSumittedLabel = data['paswordResetSumittedLabel']
+            this.modalResetPasswordTitle = data['modalResetPasswordTitle']
+            this.validUserName = Cypress.env('UI_USERNAME')
+            this.validPassword = Cypress.env('UI_PASSWORD')
+
+        })
+
+    }
+
 }
 
 module.exports = new LoginPage()
